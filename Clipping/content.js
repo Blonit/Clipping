c58d4d4936bb09f03194c0cd124ece61467092b8
Meries <127718665+Meries66@users.noreply.github.com>
2024-12-17 22:01:08 +0900

    Update content.js

--- Clipping/content.js
+++ Clipping/content.js
@@ -1,7 +1,7 @@
 document.addEventListener("copy", () => {
     navigator.clipboard.readText()
       .then((text) => {
-        const trimmedText = text.trim(); // 텍스트를 한 번만 트림
+        const trimmedText = text.trim();
         if (!trimmedText) {
           console.warn("Clipboard text is empty or whitespace.");
           return;
@@ -14,16 +14,15 @@ document.addEventListener("copy", () => {
   
           console.log("Current clipboard history:", currentHistory);
   
-          // 객체 배열로 관리
           const isDuplicate = currentHistory.some(item => item.text === trimmedText);
   
           if (!isDuplicate) {
             const newItem = {
               text: trimmedText,
-              date: new Date().toISOString(), // 저장 시간을 추가
+              date: new Date().toISOString(),
             };
   
-            currentHistory.unshift(newItem); // 최신 항목을 앞에 추가
+            currentHistory.unshift(newItem);
   
             chrome.storage.local.set({ clipboardHistory: currentHistory }, () => {
               console.log("Clipboard item added:", newItem);
